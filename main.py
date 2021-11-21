c3f8c059c4ea2a9fd34ef3d54d3bb04a8eab2d94
Redmorgan <williamw1@hotmail.co.uk>
2021-11-21 12:44:09 +0000

    Built supply and fulfillment page

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import logging
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, session
 import json
 import requests
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '6ccdb527e50c20c5deb736d0d090f584'
 
-@app.route('/')
-@app.route('/store')
+@app.route('/', methods = ['POST','GET'])
+@app.route('/store', methods = ['POST','GET'])
 def home():
     # Cloud function for collecting the product data from mongoDB
     url = "https://europe-west2-teak-amphora-328909.cloudfunctions.net/collectProducts"
     req = requests.get(url, headers={"Content-type": "application/json", "Accept": "text/plain"})
 
+    if request.method == 'POST':
+        if "buy" in request.form:
+            quantity = request.form.get('amount')
+            productID = request.form.get('productID')
+            print(quantity)
+            print(productID)
+
+            basketItem = {'productID':productID, 'quantity':quantity}
+
+            if not session.get("basket") is None:
+                session['basket'] = session['basket'].extend([basketItem])
+                print("here")
+            else:
+                session['basket'] = [basketItem]
+                print("there")
+
     return render_template('home.html', products=req.json())
 
 @app.route('/checkout')
 def checkout():
-    return render_template('checkout.html')
+    return render_template('checkout.html')#, basket = session['basket'])
 
 @app.route('/ordermanager')
 def about():
