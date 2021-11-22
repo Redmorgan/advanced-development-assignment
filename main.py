82302e3ebc0d08a6e5bddf496365f69471e4682c
Redmorgan <williamw1@hotmail.co.uk>
2021-11-22 15:58:35 +0000

    feat(order history and login): order history page progress made and login overlay added

--- main.py
+++ main.py
@@ -32,34 +32,56 @@ def home():
 
     return render_template('home.html', products=req.json())
 
+# User Checkout
 @app.route('/checkout')
 def checkout():
     return render_template('checkout.html')#, basket = session['basket'])
 
+# Admin Order Manager
 @app.route('/ordermanager')
 def orderManager():
 
     # Get all order information
 
-    return render_template('orderManager.html')
+    return render_template('orderManager.html', role="admin")
 
+# Admin Order Manager (Single order)
 @app.route('/ordermanager/<orderNumber>')
 def orderView(orderNumber):
 
     # Cloud function to get order details using 'orderNumber'
 
-    return render_template('orderView.html')
+    return render_template('orderView.html', role="admin")
 
+# User Order History
+@app.route('/orderhistory')
+def orderHistory():
+
+    # Get all order information
+
+    return render_template('orderManager.html', role="user")
+
+# User Order History (Single Order)
+@app.route('/orderhistory/<orderNumber>')
+def orderHistoryView(orderNumber):
+
+    # Cloud function to get order details using 'orderNumber'
+
+    return render_template('orderView.html', role="user")
+
+# Admin Controls
 @app.route('/admin')
 def form():
     return render_template('admin.html')
 
+# 500 Error response
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
+# 404 Error Response
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
